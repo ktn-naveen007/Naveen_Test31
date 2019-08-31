6b6ef0bbddd7492ee7269136ac1a11dbd58b8e92
Navin Tirumal Kotamraju <navin.kotamraju@yash.com>
2019-08-31 17:02:37 +0530

    01

---
+++ src/test/java/com/automation/Scripts/Invoke.java
@@ -0,0 +1,22 @@
+package com.automation.Scripts;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Invoke {
+
+	@Test
+	public void invokeApp(){
+		// TODO Auto-generated method stub
+		
+		System.setProperty("webdriver.driver.chromedriver", "./Drivers/chromedriver.exe");
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.amazon.in");
+
+		driver.close();
+	}
+
+}
